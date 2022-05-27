11858a951e9c8346db0056413073293a18d81304
rayankatbi <rayanarabikatbi@gmail.com>
2022-05-27 16:59:36 +0300

    add schedule screen

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Style {
   static const Color white = Colors.white;
   static const Color darkMov = Color(0xff272430);
-  static const Color mov = Color(0xff3C1F7B); 
+  static const Color mov = Color(0xff3C1F7B);
   static const Color red = Color(0xffFF636C);
   static const Color lightMov = Color(0xffCCC2FE);
   static const Color cardColor = Color(0xff7E64FF);
